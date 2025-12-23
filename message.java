b779e8a57d1e9e464d56ab2274fe31e27f0e7de6
Kushagra-Sr44 <kushagrasrivastava870@gmail.com>
2025-12-24 01:37:59 +0530

    Add files via upload

---
+++ message.java
@@ -0,0 +1,18 @@
+package com.myServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class message extends HttpServlet{
+public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+	String n=req.getParameter("name");
+	PrintWriter out=res.getWriter();
+	out.print("Hello "+n);
+	
+}
+}
+ 
